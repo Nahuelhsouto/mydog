534e49ef23e01dad850207029d65db178c2e8a72
nahuelhsouto <nahuelhsouto@gmail.com>
2022-02-04 01:16:54 -0300

    Interface adjustment

--- template/header.php
+++ template/header.php
@@ -24,7 +24,7 @@
 	                    <span> </span>
 	                </button>
 					
-					<div class="collapse navbar-collapse py-3 py-lg-0" id="navigation">
+					<div class="navbar-collapse py-3 py-lg-0 collapse" id="navigation" style>
 						
 						<ul class="navbar-nav ms-lg-auto">
 							<li class="nav-item me-lg-4">
@@ -60,4 +60,9 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="offcanvas.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.8/highlight.min.js"></script>    
+
+<script src="test/popper.min.js"></script>
+<script src="test/bootstrap.min.js"></script>  
+
+<script src="offcanvas.js"></script>
