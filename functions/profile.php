7a576fcbb7b6dde820ebd02aa879b1b7bcac55c7
nahuelhsouto <nahuelhsouto@gmail.com>
2022-02-26 02:51:18 -0300

    Profile ready

--- functions/profile.php
+++ functions/profile.php
@@ -2,26 +2,48 @@
 
 $userId = $_POST['id'];
 
+
 include("../admin/config/db.php");
 $qsql=$connection->prepare("SELECT * FROM usuarios WHERE id = $userId");
 $qsql->execute();
 $userList=$qsql->fetchALL(PDO::FETCH_ASSOC);
 
 
+
+
 foreach($userList as $user) { 
     
 
   echo " 
-  <form action='#' method='POST' enctype='multipart/form-data' autocomplete='off'>
-  <img src='../pictures/foto.jpg' alt=''>
-  <input type='file' class='form-control' id='txtFoto'>
+  <form  method='POST' enctype='multipart/form-data' autocomplete='off'>
+  
+  
 
-  <input class='inputs' type='text' value='".$user['username']."' id='userName'> 
-  <input class='inputs' type='text' value='".$user['mail']."' id='userMail'>
-  <input class='inputs' type='text' value='".$user['direccion']."' id='userDire'>
+  <div class='cont-inp'>
+<input type='file' name='txtFoto' id='txtFoto' class='inputfile inputfile-6' data-multiple-caption='{count} archivos seleccionados' multiple />
+<label for='txtFoto'>
+<figure>
+<img src='../pictures/".$user['foto']."' alt=''>
+</figure>
 
-  <input class='save' type='file' name='image' accept='image/x-png,image/gif,image/jpeg,image/jpg>'
+</label>
+</div>
+
+";
+ echo"
+ 
+ <span>Nombre de usuario</span>
+  <input class='inputs' type='text' value='".$user['username']."' id='userName' name='userName'> 
+ <span>Mail</span>
+  <input class='inputs' type='text' value='".$user['mail']."' id='userMail' name='userMail'>
+ <span>Dirección</span>
+  <input class='inputs' type='text' value='".$user['direccion']."' id='userDire' name='userDire'>
+
+  <input class='save' type='submit' name='send' value='save' /> 
   </form>
 ";
+
 }
+
+
 ?>
