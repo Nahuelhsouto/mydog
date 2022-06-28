3518f578ce214b2a520257f5cff40a93aac3d07f
nahuelhsouto <nahuelhsouto@gmail.com>
2022-06-27 21:22:37 -0300

    Re-design perdidos

--- admin/template/header.php
+++ admin/template/header.php
@@ -30,8 +30,8 @@ session_start();
     <link rel="stylesheet" href="../css/user.css">
     <link rel="stylesheet" href="../css/demo.css">
     <link rel="stylesheet" href="../css/cs-select.css">
-    <link rel="stylesheet" href="../css/cs-skin-circular.css">
     <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/cs-skin-circular.css">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
